fe37d54734a17f8956ace901f829a230c5e27c1d
plzswitchoff <ara3ng@gmail.com>
2025-10-15 18:26:14 +0900

    회원가입 페이지 수정중

--- frontend/src/pages/SigninInput.jsx
+++ frontend/src/pages/SigninInput.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const SigninInput = ({title, type}) => {
+const SigninInput = ({title, type, placeholder}) => {
     return (
     <>
       <div className=" flex justify-end-safe items-center">
         <div className='mr-3 mb-3'>{title}</div>
         <div>
-          <input type={type} className="w-[300px] bg-white rounded-md shadow-md focus:shadow-lg focus:outline-none p-1 m-2"/>
+          <input type={type} placeholder={placeholder} className="w-[300px] bg-white rounded-md shadow-md focus:shadow-lg focus:outline-none p-1 m-2"/>
         </div>
       </div>
     </>
--- frontend/src/pages/Signup/index.jsx
+++ frontend/src/pages/Signup/index.jsx
@@ -1,30 +1,120 @@
+import { useNavigate } from "react-router-dom";
 import SigninInput from "../SigninInput";
+import { useState } from "react";
 
 export const Signup = () => {
+  const navigation = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const [error, setError] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  //테스트용 이름들
+  const existingNames = ["연서", "가현", "예원"];
+
+  const validate = () => {
+    const newErrors = {};
+
+    if (existingNames.includes(form.name)) {
+      newErrors.name = "이미 사용중인 이름입니다.";
+    }
+
+    if (/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i.test(form.email)) {
+      newErrors.email = "올바른 이메일 형식이 아닙니다.";
+    }
+
+    if (!/^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/.test(form.password)) {
+      newErrors.password = "영문자와 숫자를 포함해 8자 이상이어야 합니다.";
+    }
+
+    if (form.password !== form.passwordConfirm) {
+      newErrors.passwordConfirm = "비밀번호가 일치하지 않습니다";
+    }
+
+    setError(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
   const handlesubmit = (e) => {
     e.preventDefault();
+    if (validate()) {
+      alert("회원가입이 완료되었습니다");
+      console.log("버튼누름")
+      navigation("/");
+    }
   };
   return (
     <>
       <div className="mt-10  flex-col items-center justify-center">
         <div className="mb-10 text-custom-basic-font text-3xl">Signup</div>
 
-        <form className="flex-col items-center justify-center" onSubmit={handlesubmit}>
-          <SigninInput title="이름" type="name" name="name" />
+        <form
+          className="flex-col items-center justify-center"
+          onSubmit={handlesubmit}
+        >
+          <SigninInput
+            title="name"
+            type="text"
+            name="name"
+            placeholder="이름을 입력해주세요"
+            value={form.name}
+            onChange={handleChange}
+          />
+          {error.name && (
+            <p className="text-red-500 text-sm mt-1">{error.name}</p>
+          )}
           <div className="">
-            <SigninInput title="이메일" type="email" name="email" />
-            <SigninInput title="비밀번호" type="password" name="password" />
             <SigninInput
-              title="비밀번호 확인"
-              type="passwordConfirm"
-              name="password confirm"
+              title="email"
+              type="email"
+              name="email"
+              placeholder="이메일을 입력해주세요"
+              value={form.email}
+              onChange={handleChange}
             />
+            {error.email && (
+              <p className="text-red-500 text-sm mt-1">{error.email}</p>
+            )}
+            <SigninInput
+              title="pw"
+              type="password"
+              name="password"
+              placeholder="비밀번호를 입력해주세요"
+              value={form.password}
+              onChange={handleChange}
+            />
+            {error.password && (
+              <p className="text-red-500 text-sm mt-1">{error.password}</p>
+            )}
+            <SigninInput
+              title="pw confirm"
+              type="password"
+              name="passwordConfirm"
+              placeholder="비밀번호를 다시 입력해주세요"
+              value={form.passwordConfirm}
+              onChange={handleChange}
+            />
+            {error.passwordConfirm && (
+              <p className="text-red-500 text-sm mt-1">
+                {error.passwordConfirm}
+              </p>
+            )}
             <div className="mt-5">
               <button
-                type="submit"
+                onClick={() => navigation("/")}
+                type="button"
                 className="bg-custom-search mr-3 w-[80px] h-[30px]"
               >
-                Cancle
+                Cancel
               </button>
               <button
                 type="submit"
